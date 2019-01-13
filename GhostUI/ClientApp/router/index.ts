f1c5205eeaa7d756dc2dd1575a2860da8e4257fe
Matt Areddia <mareddia@protonmail.com>
2019-01-13 14:49:04 -0600

    vue-router refactoring

---
+++ GhostUI/ClientApp/router/index.ts
@@ -0,0 +1,12 @@
+﻿import Vue from 'vue';
+import { routes } from './routes';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
+export default router;
