817af9f05e65d2c849d3981711df80eddba9036b
Matt Areddia <mareddia@protonmail.com>
2019-02-21 14:25:02 -0600

    NavBar.html.vue component and style refactoring

--- GhostUI/ClientApp/components/nav/navbar/NavBar.html.vue
+++ GhostUI/ClientApp/components/nav/navbar/NavBar.html.vue
@@ -1,24 +1,22 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="brand-wrapper">
-            <img src="../../../assets/image/bulma.io-logo.png" alt="">
-        </div>
-        <div class="navbar-centered">
-            <div v-if="showNavigation" class="navbar-start">
+        <div class="navbar-wrapper">
+            <div class="brand-wrapper">
+                <img src="../../../assets/image/bulma.io-logo.png" alt="" width="165">
+            </div>
+            <div v-if="showNavigation" class="navbar-routes">
                 <router-link :to="routesConfig.Form.path" class="navbar-item">
                     <span class="icon">
                         <font-awesome-icon :icon="routesConfig.Form.meta.icon" />
                     </span>
                     <span>{{routesConfig.Form.displayName}}</span>
                 </router-link>
-                <div class="nav-divider"></div>
                 <router-link :to="routesConfig.Dashboard.path" class="navbar-item">
                     <span class="icon">
                         <font-awesome-icon :icon="routesConfig.Dashboard.meta.icon" />
                     </span>
                     <span>{{routesConfig.Dashboard.displayName}}</span>
                 </router-link>
-                <div class="nav-divider"></div>
                 <router-link :to="routesConfig.FetchData.path" class="navbar-item">
                     <span class="icon">
                         <font-awesome-icon :icon="routesConfig.FetchData.meta.icon" />
@@ -26,7 +24,8 @@
                     <span>{{routesConfig.FetchData.displayName}}</span>
                 </router-link>
             </div>
-        </div>     
+            <div v-else class="navbar-routes"></div>
+        </div>
     </nav>
 </template>
 
