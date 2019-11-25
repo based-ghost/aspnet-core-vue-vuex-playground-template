c10ce180f62e28a2c59e48642cb7241549f288ee
Matt Areddia <mareddia@protonmail.com>
2019-11-24 19:45:52 -0600

    (Fix) - Potential security issue - Appending "noreferrer" to rel attribute on anchor elements where target = "_blank".

--- GhostUI/ClientApp/src/components/Settings.vue
+++ GhostUI/ClientApp/src/components/Settings.vue
@@ -14,7 +14,7 @@
           <a
             class="dropdown-item"
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
             :href="nugetUrls.HEALTH_UI"
             role="button"
           >
@@ -25,7 +25,7 @@
           <a
             class="dropdown-item"
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
             :href="nugetUrls.SWAGGER_DOCS"
             role="button"
           >
--- GhostUI/ClientApp/src/views/Dashboard/Dashboard.vue
+++ GhostUI/ClientApp/src/views/Dashboard/Dashboard.vue
@@ -3,9 +3,9 @@
     <section class="hero is-dark">
       <div class="hero-body">
         <div class="is-flex is-horizontal-center">
-          <img 
-            src="@/assets/image/aspcore-vue.png" 
-            alt="" 
+          <img
+            src="@/assets/image/aspcore-vue.png"
+            alt="AspNetCoreVue"
             width="300"
           />
         </div>
@@ -25,7 +25,7 @@
                       <a
                         class="dashboard-link vue"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://vuejs.org/"
                       >Vue</a>
                       is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. Vue is also perfectly capable of powering sophisticated Single-Page Applications when combined with other libraries.
@@ -34,7 +34,7 @@
                       <a
                         class="dashboard-link vuex"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://vuex.vuejs.org/"
                       >Vuex</a>
                       serves as a centralized store for all the components in an application, with rules ensuring that the state can only be mutated in a predictable fashion.
@@ -43,7 +43,7 @@
                       <a
                         class="dashboard-link bulma"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://bulma.io/"
                       >Bulma</a>
                       is open source CSS framework based on Flexbox (with no JQuery dependency).
@@ -58,7 +58,7 @@
                       <a
                         class="dashboard-link aspcore"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://www.asp.net/"
                       >ASP.NET Core</a>
                       is an open source web framework for building modern web apps and services with .NET. Creates websites based on HTML5, CSS, and JavaScript that are simple, fast, and can scale to millions of users.
@@ -67,7 +67,7 @@
                       <a
                         class="dashboard-link sass"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://sass-lang.com/"
                       >SASS</a>
                       is a CSS pre-processor extension to help provide more flexibility &amp; maintainability to your style-sheets. Use nesting, variables, mixins, inheritance and more great features to make writing CSS a less arduous task.
@@ -76,7 +76,7 @@
                       <a
                         class="dashboard-link typescript"
                         target="_blank"
-                        rel="noopener"
+                        rel="noopener noreferrer"
                         href="https://www.typescriptlang.org/"
                       >TypeScript</a>
                       is a typed superset of JavaScript that compiles to plain JavaScript
