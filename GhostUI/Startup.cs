50315e2f6aa5696e4de15fc0066574f90159ea2c
Matt Areddia <mareddia@protonmail.com>
2020-01-22 23:35:41 -0600

    Fix for issue Swagger Supporting UI Files Not Loading

--- GhostUI/Startup.cs
+++ GhostUI/Startup.cs
@@ -98,11 +98,8 @@ namespace GhostUI
                 });
             });
 
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-            app.UseRouting();
-
             app.UseCors(_corsPolicyName);
+            app.UseStaticFiles();
 
             // Register the Swagger generator and the Swagger UI middlewares
             // NSwage.MsBuild + adding automation config in GhostUI.csproj makes this part of the build step (updates to API will be handled automatically)
@@ -112,6 +109,9 @@ namespace GhostUI
                 settings.Path = "/docs";
                 settings.DocumentPath = "/docs/api-specification.json";
             });
+
+            app.UseSpaStaticFiles();
+            app.UseRouting();
 
             // Map controllers / SignalR hubs / HealthChecks
             // Configure VueCliMiddleware package to handle startup of Vue.js ClientApp front-end
